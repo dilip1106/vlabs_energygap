3afaeee0cfb79c4acef04e42a424f9206dab1fca
dilip1106 <dilipteli38@gmail.com>
2024-07-24 22:48:29 +0530

    table values

--- indexPhy4.js
+++ indexPhy4.js
@@ -125,12 +125,19 @@ function checkbtnPressed(btnNum) {
   }
 }
 
+// let curarr = [
+//   6.83, 7.07, 7.31, 7.56, 7.82, 8.09, 8.36, 8.64, 8.92, 9.22, 9.52, 9.833, 10.1,
+//   10.47, 10.81, 11.15, 11.5, 11.86, 12.23, 12.61, 12.99, 13.39, 13.79, 14.21,
+//   14.63, 15.07, 15.51, 15.97, 16.44, 16.91, 17.4, 17.9, 18.41, 18.93, 19.46,
+//   20.0, 20.56, 21.13, 21.71,22.10,22.51,23.01,23.60
+// ];
+
 let curarr = [
-  6.83, 7.07, 7.31, 7.56, 7.82, 8.09, 8.36, 8.64, 8.92, 9.22, 9.52, 9.833, 10.1,
-  10.47, 10.81, 11.15, 11.5, 11.86, 12.23, 12.61, 12.99, 13.39, 13.79, 14.21,
-  14.63, 15.07, 15.51, 15.97, 16.44, 16.91, 17.4, 17.9, 18.41, 18.93, 19.46,
-  20.0, 20.56, 21.13, 21.71,22.10,22.51,23.01,23.60
-];
+  7.83, 8.46, 9.01, 10.3, 11.8, 13.8, 15.2, 17.8, 19.5, 22.4, 24.6, 27.2,28.9, 32.4, 
+  36.5, 40.8, 44.3, 48.1, 52.2, 56.1, 60.9, 64.2, 68.13, 72.4, 77.9, 85.3, 91.4, 
+  99.5,102.4,109.5,116.7,123.5,126.6,138.7,145.4,153.6,158.7,165.4,171.3,175.8];
+
+// let curarr = [];
 
 let curno = 6.83;
 let temprature = 27.0;
@@ -147,16 +154,56 @@ function startWorking() {
     temprature++;
     temptext.textContent = temprature.toFixed(2);
     if (flag == 0) {
-      curno = curarr[i++] + getRndInteger(0.1, 0.3);
+      curno = curarr[i++] + getRndInteger(0.5, 1.5);
       flag = 1;
     } else {
-      curno = curarr[i++] - getRndInteger(0.1, 0.3);
+      curno = curarr[i++] - getRndInteger(0.5, 1.5);
       flag = 0;
     }
+    // if(temprature < 30){
+    //   curno = getRndInteger(8.0,9.9);
+    // }else if(temprature < 33 ) {
+    //   curno = getRndInteger(10.0,9.9);
+    // }else if(temprature < 35 ) {
+    //   curno = getRndInteger(8.0,9.9);
+    // }else if(temprature < 37 ) {
+    //   curno = getRndInteger(8.0,9.9);
+    // }else if(temprature < 39 ) {
+    //   curno = getRndInteger(8.0,9.9);
+    // }else if(temprature < 40 ) {
+    //   curno = getRndInteger(8.0,9.9);
+    // }else if(temprature < 41 ) {
+    //   curno = getRndInteger(8.0,9.9);
+    // }else if(temprature < 43 ) {
+    //   curno = getRndInteger(8.0,9.9);
+    // }else if(temprature < 45 ) {
+    //   curno = getRndInteger(8.0,9.9);
+    // }else if(temprature < 47 ) {
+    //   curno = getRndInteger(8.0,9.9);
+    // }else if(temprature < 49 ) {
+    //   curno = getRndInteger(8.0,9.9);
+    // }else if(temprature < 50 ) {
+    //   curno = getRndInteger(8.0,9.9);
+    // }else if(temprature < 53 ) {
+    //   curno = getRndInteger(8.0,9.9);
+    // }else if(temprature < 55 ) {
+    //   curno = getRndInteger(8.0,9.9);
+    // }else if(temprature < 58 ) {
+    //   curno = getRndInteger(8.0,9.9);
+    // }else if(temprature < 60 ) {
+    //   curno = getRndInteger(8.0,9.9);
+    // }else if(temprature < 62 ) {
+    //   curno = getRndInteger(8.0,9.9);
+    // }else if(temprature < 63 ) {
+    //   curno = getRndInteger(8.0,9.9);
+    // }else if(temprature < 66 ) {
+    //   curno = getRndInteger(8.0,9.9);
+    // }
     if(temprature%5===0.00){
       filldata(srno,temprature,curno)
       srno++
       console.log("fill data callede");
+      console.log(curarr)
     }
     curtext.textContent = curno.toFixed(2);
     // Stop the interval after reaching a certain temprature
@@ -165,7 +212,7 @@ function startWorking() {
       document.getElementById("hotburner").style.display = "none";
       // startdroping();
     }
-  }, 1000); // Increase temprature every 100 milliseconds
+  }, 100); // Increase temprature every 100 milliseconds
 }
 
 function startdroping() {
